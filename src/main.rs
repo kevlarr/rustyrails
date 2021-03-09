cd704902081ef8967d559bdfec55b063c311e332
kevlarr <6306496+kevlarr@users.noreply.github.com>
2021-03-08 23:50:24 -0500

    Note to other branch

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,20 @@ async fn main() -> io::Result<()> {
     let app = || {
         let routes = Router::new("/")
             .get("", root)
-            .get("{name}", greet)
+
+            // Ultimately I would like to be able to validate that handlers
+            // in the scope itself conform to a certain signature with a
+            // known scope extractor, eg..
+            //
+            // .scope::<NameScope>("{name}", |s| s
+            //     .get("", greet)
+            // )
+            //
+            // ... but it's over my head so far. See:
+            // https://github.com/kevlarr/rustyrails/tree/scoped-handler-sig-validation
+            .scope("{name}", |name| name
+                .get("", greet)
+            )
             .service;
 
         App::new().service(routes)
